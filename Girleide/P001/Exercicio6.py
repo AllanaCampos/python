e11fe30f700862692f098d548573fc7eb7a5be50
girleide <girleide.macario@gmail.com>
2023-11-24 20:20:59 -0300

    alteração no exercício 6

--- Girleide/P001/Exercicio6.py
+++ Girleide/P001/Exercicio6.py
@@ -5,3 +5,9 @@ print(L[:-1:])
 print(L[::-2])
 print(L[-2::])
 print(L[:-2:])
+
+anoNasc = int (input("Digite o ano de nascimento"))
+signos = ['Macaco', 'Galo', 'Cão', 'Porco', 'Rato', 'Boi', 'Tigre', 'Coelho', 'Dragão', 'Serpente', 'Cavalo', 'Cabra']
+anoMacaco = 0
+valor = (anoNasc - anoMacaco) % 12
+print(f"Seu signo no zodíaco chines e: {signos[valor]}")
